f8d7c46e053c6cff194f9c7deb760282c08f64da
dormon <imilet@fit.vutbr.cz>
2020-11-24 16:08:52 +0100

    geGL modification

--- src/main.cpp
+++ src/main.cpp
@@ -50,76 +50,15 @@ GLuint createTexture(std::string const&fileName){
 
   stbi_image_free(pixels);
 
-  glTextureParameteri(tex,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+  glGenerateTextureMipmap(tex);
+
+  glTextureParameteri(tex,GL_TEXTURE_MIN_FILTER,GL_LINEAR_MIPMAP_LINEAR);
   glTextureParameteri(tex,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
   glTextureParameteri(tex,GL_TEXTURE_WRAP_S,GL_REPEAT);
   glTextureParameteri(tex,GL_TEXTURE_WRAP_T,GL_REPEAT);
 
   return tex;
 }
-
-GLuint createShader(GLenum type,std::string const&src){
-  GLuint id = glCreateShader(type);
-  char const*const srcs [] = {
-    src.data()
-  };
-  glShaderSource(id,1,srcs,nullptr);
-  glCompileShader(id);
-
-
-  GLint status;
-  glGetShaderiv(id,GL_COMPILE_STATUS,&status);
-
-  if(status != GL_TRUE){
-    GLint len;
-    glGetShaderiv(id,GL_INFO_LOG_LENGTH,&len);
-
-    char*buffer = new char[len];
-
-    glGetShaderInfoLog(id,len,nullptr,buffer);
-
-    auto msg = "shader compilation failed: "+std::string(buffer);
-    delete[]buffer;
-
-    throw std::runtime_error(msg);
-
-  }
-
-  return id;
-}
-
-GLuint createProgram(std::vector<GLuint> const&shaders){
-  GLuint prg = glCreateProgram();
-
-  for(auto const&shader:shaders)
-    glAttachShader(prg,shader);
-
-  glLinkProgram(prg);
-
-  GLint status;
-  glGetProgramiv(prg,GL_LINK_STATUS,&status);
-
-  if(status != GL_TRUE){
-    GLint len;
-    glGetProgramiv(prg,GL_INFO_LOG_LENGTH,&len);
-
-    char*buffer = new char[len];
-
-    glGetProgramInfoLog(prg,len,nullptr,buffer);
-
-    auto msg = "program linking failed: "+std::string(buffer);
-    delete[]buffer;
-
-    throw std::runtime_error(msg);
-  }
-
-  return prg;
-}
-
-void printError(GLenum source,GLenum type, GLuint id,GLenum severity, GLsizei length, const GLchar *message,const GLvoid*userParam){
-  std::cerr << "My Error: " << message << std::endl;
-}
-
 
 int main(int argc,char*argv[]){
   //SDL2 glfw glaux QT ...
@@ -139,16 +78,8 @@ int main(int argc,char*argv[]){
 
   ge::gl::init();
 
-  glDebugMessageCallback(printError,nullptr);
-
-  //ge::gl::setDefaultDebugMessage();
+  ge::gl::setDefaultDebugMessage();
  
-  //Empty Vertex Array (empty Vertex Puller setting)
-
-  
-  GLuint vbo;
-  glCreateBuffers(1,&vbo);
-
 
   struct Vertex{
     glm::vec3 position;
@@ -183,7 +114,7 @@ int main(int argc,char*argv[]){
     }
   }
 
-  glNamedBufferData(vbo,sizeof(Vertex)*vertices.size(),vertices.data(),GL_DYNAMIC_COPY);
+  auto vbo = std::make_shared<ge::gl::Buffer>(vertices);
 
 
   struct Face{
@@ -216,53 +147,23 @@ int main(int argc,char*argv[]){
     }
   }
 
-  //for(auto &f:faces)
-  //  f.print();
-
-  GLuint ebo;
-  glCreateBuffers(1,&ebo);
-  glNamedBufferData(ebo,sizeof(Face)*faces.size(),faces.data(),GL_DYNAMIC_COPY);
-
-  GLuint vao;
-  glCreateVertexArrays(1,&vao);
+  auto ebo = std::make_shared<ge::gl::Buffer>(faces);
 
 
-  //position
-  glEnableVertexArrayAttrib (vao,0);
-  glVertexArrayAttribFormat (vao,0,3,GL_FLOAT,GL_FALSE,0);
-  glVertexArrayVertexBuffer (vao,0,vbo,sizeof(float)*0,sizeof(Vertex));
-  glVertexArrayAttribBinding(vao,0,0);
+  auto vao = std::make_shared<ge::gl::VertexArray>();
+  vao->addAttrib(vbo,0,3,GL_FLOAT,sizeof(Vertex),sizeof(float)*0);
+  vao->addAttrib(vbo,1,3,GL_FLOAT,sizeof(Vertex),sizeof(float)*3);
+  vao->addAttrib(vbo,2,2,GL_FLOAT,sizeof(Vertex),sizeof(float)*6);
+  vao->addElementBuffer(ebo);
 
-  //normal
-  glEnableVertexArrayAttrib (vao,1);
-  glVertexArrayAttribFormat (vao,1,3,GL_FLOAT,GL_FALSE,0);
-  glVertexArrayVertexBuffer (vao,1,vbo,sizeof(float)*3,sizeof(Vertex));
-  glVertexArrayAttribBinding(vao,1,1);
-
-  //tex coord
-  glEnableVertexArrayAttrib (vao,2);
-  glVertexArrayAttribFormat (vao,2,2,GL_FLOAT,GL_FALSE,0);
-  glVertexArrayVertexBuffer (vao,2,vbo,sizeof(float)*6,sizeof(Vertex));
-  glVertexArrayAttribBinding(vao,2,2);
-
-  glVertexArrayElementBuffer(vao,ebo);
-
-  GLuint prg;
-  prg = createProgram({
-      createShader(GL_VERTEX_SHADER  ,"#version 460\n" + loadTxtFile("../shaders/earth.vp")),
-      createShader(GL_FRAGMENT_SHADER,"#version 460\n" + loadTxtFile("../shaders/lightingFunctions.vp") + loadTxtFile("../shaders/earth.fp"))
-      });
+  auto prg = std::make_shared<ge::gl::Program>(
+      std::make_shared<ge::gl::Shader>(GL_VERTEX_SHADER  ,"#version 460\n",loadTxtFile("../shaders/earth.vp")),
+      std::make_shared<ge::gl::Shader>(GL_FRAGMENT_SHADER,"#version 460\n",loadTxtFile("../shaders/lightingFunctions.vp"), loadTxtFile("../shaders/earth.fp"))
+      );
+  prg->setNonexistingUniformWarning(false);
 
   //locations
-  GLuint saturationLocation = glGetUniformLocation(prg,"saturation");
 
-  GLuint modelMatrixLocation      = glGetUniformLocation(prg,"modelMatrix"     );
-  GLuint viewMatrixLocation       = glGetUniformLocation(prg,"viewMatrix"      );
-  GLuint projectionMatrixLocation = glGetUniformLocation(prg,"projectionMatrix");
-  GLuint shadingTypeLocation      = glGetUniformLocation(prg,"shadingType"     );
-  GLuint lightingTypeLocation     = glGetUniformLocation(prg,"lightingType"    );
-
-  float saturation = 0.5f;
   glm::vec3 position = glm::vec3(0.f);
   float scale[2]    = {1.f,1.f};
   float alpha       = 0.f;
@@ -274,9 +175,6 @@ int main(int argc,char*argv[]){
   bool wireframe = false;
 
   bool running = true;
-
-  int  shadingType = 0;
-  bool lambertLighting = false;
 
   auto tex = createTexture("../images/earth.png");
 
@@ -291,9 +189,6 @@ int main(int argc,char*argv[]){
       if(event.type == SDL_QUIT)
         running = false;
       if(event.type == SDL_KEYDOWN){
-        if(event.key.keysym.sym == SDLK_o)saturation += 0.01f;
-        if(event.key.keysym.sym == SDLK_p)saturation -= 0.01f;
-        glProgramUniform1f(prg,saturationLocation,saturation);
 
         if(event.key.keysym.sym == SDLK_w)position[2] += 0.01;
         if(event.key.keysym.sym == SDLK_s)position[2] -= 0.01;
@@ -310,10 +205,6 @@ int main(int argc,char*argv[]){
         if(event.key.keysym.sym == SDLK_q)alpha += 0.003;
         if(event.key.keysym.sym == SDLK_e)alpha -= 0.003;
 
-        if(event.key.keysym.sym == SDLK_k){shadingType--;if(shadingType<0)shadingType=0;}
-        if(event.key.keysym.sym == SDLK_l){shadingType++;if(shadingType>2)shadingType=2;}
-        if(event.key.keysym.sym == SDLK_n)lambertLighting = !lambertLighting;
-
         if(event.key.keysym.sym == SDLK_m)wireframe = !wireframe;
 
         auto T = glm::translate(glm::mat4(1.f),position                              );
@@ -322,8 +213,7 @@ int main(int argc,char*argv[]){
 
         auto modelMatrix = T*R*S;
 
-        glProgramUniformMatrix4fv(prg,modelMatrixLocation,1,GL_FALSE,(float*)&modelMatrix);
-
+        prg->setMatrix4fv("modelMatrix",(float*)&modelMatrix);
        
       }
       if(event.type == SDL_MOUSEMOTION){
@@ -338,11 +228,8 @@ int main(int argc,char*argv[]){
       }
     }
 
-    glProgramUniform1i(prg,shadingTypeLocation,shadingType);
-    glProgramUniform1i(prg,lightingTypeLocation,(int)lambertLighting);
-
     auto projectionMatrix = glm::perspective(glm::half_pi<float>(),(float)windowWidth / (float)windowHeight,0.1f,1000.f);
-    glProgramUniformMatrix4fv(prg,projectionMatrixLocation,1,GL_FALSE,(float*)&projectionMatrix);
+    prg->setMatrix4fv("projectionMatrix",(float*)&projectionMatrix);
 
 
     glm::vec3 camPosition;
@@ -351,32 +238,27 @@ int main(int argc,char*argv[]){
     camPosition.z = camDistance*glm::sin(camXAngle)*glm::cos(camYAngle);
 
     auto viewMatrix = glm::lookAt(camPosition,glm::vec3(0,0,0),glm::vec3(0,1,0));
-    glProgramUniformMatrix4fv(prg,viewMatrixLocation,1,GL_FALSE,(float*)&viewMatrix);
+    prg->setMatrix4fv("viewMatrix",(float*)&viewMatrix);
 
 
     glEnable(GL_DEPTH_TEST);
     glClearColor(0.1,0.1,0.1,1);
-    //glClearColor(0,0,0,1);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    glBindVertexArray(vao);
-
-    glPointSize(10);
+    vao->bind();
 
     if(wireframe)
       glPolygonMode(GL_FRONT_AND_BACK,GL_LINE);
     else
       glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
 
-    glUseProgram(prg);
+    prg->use();
     glDrawElements(GL_TRIANGLES,nx*ny*2*3,GL_UNSIGNED_INT,nullptr);
-    //glDrawArrays(GL_POINTS,0,nx*ny);
 
-    glBindVertexArray(0);
+    vao->unbind();
 
     SDL_GL_SwapWindow(window);
 
   }
-
   return 0;
 }
